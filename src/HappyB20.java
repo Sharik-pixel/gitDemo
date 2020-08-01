0a89c29d750e010b12132171d56e7a4c37f1099c
Sharik-pixel <68974889+Sharik-pixel@users.noreply.github.com>
2020-08-01 16:35:06 -0400

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -4,7 +4,8 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is fun");
 
-
+        System.out.println("B20 August");
+        
         // a brand new line is added
 
     }
